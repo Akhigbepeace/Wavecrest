95a995c965b54e9561e447d80b28a6af3cb02c90
Akhigbepeace <akhigbepeace2005@gmail.com>
2022-06-08 20:46:59 +0100

    added chakra UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/layouts/Navbar';
-import Home from './components/Pages/Home';
-import About from './components/Pages/About';
-import './App.css';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/layouts/Navbar";
+import Home from "./components/Pages/Home";
+import About from "./components/Pages/About";
+import "./main.scss";
+// import "./App.css";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <div id='container'>
-          <Routes>
-            <Route exact path='/home' component={Home}/>
-            <Route exact path='/about' component={About}/>
-          </Routes>
-          <Home />
-        </div>
+    
+      <Router>
+        <Fragment>
+          <div id="container">
+            <Navbar />
+            <Routes>
+              <Route exact path="/home" component={<Home />} />
+              <Route exact path="/about" component={<About />} />
+            </Routes>
+            <Home />
+          </div>
         </Fragment>
-    </Router>
+      </Router>
+   
   );
 }
 
--- src/components/DiscoverHospitality.js
+++ src/components/DiscoverHospitality.js
@@ -1,19 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const DiscoverHospitality = () => {
   return (
     <div id="discover-hospitality">
-      <div className= "brief-desc">
+      <div className="brief-desc">
         <h1>NIGERIAN'S PREMIERE HOSPITALITY MONOTECHNIC</h1>
-        <p>Wavecrest College of Hospitality is consectetur, adipisicing elit. Eveniet, facilis, ab, doloribus ipsam nisi architecto beatae optio nemo consequuntur aut dolorum at sequi tempora deleniti itaque similique reprehenderit mollitia? Nesciunt!</p>
-        <a href="#!">APPLY NOW</a>
+        <p>
+          Wavecrest College of Hospitality is consectetur adipiscing elit, sed
+          do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur.
+        </p>
+        <Link to="#!">APPLY NOW</Link>
       </div>
-      <div className ="discover-pics">
-        <div className ="first-pic">
+      <div className="discover-pics">
+        <div className="first-pic">
           <img src="assets/Images/workbench.jpg" alt="workbench" />
         </div>
 
-        <div className ="discover">
+        <div className="discover">
           <h1>Discover Hospitality</h1>
         </div>
 
@@ -21,13 +29,12 @@ const DiscoverHospitality = () => {
           <img src="assets/Images/img_ny.jpg" alt="img_ny" />
         </div>
 
-        <div className ="last-pic">
+        <div className="last-pic">
           <img src="assets/Images/coffee.jpg" alt="coffee" />
         </div>
-
       </div>
     </div>
-  )
+  );
 }
 
 export default DiscoverHospitality
