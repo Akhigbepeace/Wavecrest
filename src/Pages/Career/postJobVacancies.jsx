639eced07ac18fde02ff93fc7ccfca7c0b35253b
Akhigbepeace <akhigbepeace2005@gmail.com>
2022-07-19 20:30:43 +0100

    Reduced amount of file changes

---
+++ src/Pages/Career/postJobVacancies.jsx
@@ -0,0 +1,133 @@
+import { Heading, Input, Box, Text, Button, Textarea } from "@chakra-ui/react";
+
+import React, { Fragment } from "react";
+
+const postJobVacancies = () => {
+  const forms = [
+    {
+      fieldType: "name",
+      placeHolder: "FullName",
+      variant: "filled",
+    },
+    {
+      fieldType: "text",
+      placeHolder: "Company",
+      variant: "filled",
+    },
+    {
+      fieldType: "email",
+      placeHolder: "Email",
+      variant: "filled",
+    },
+    {
+      fieldType: "text",
+      placeHolder: "Job Description",
+      variant: "filled",
+    },
+    {
+      fieldType: "text",
+      placeHolder: "Application Deadline",
+      variant: "filled",
+    },
+    {
+      fieldType: "text",
+      placeHolder: "Application Deadline",
+      variant: "filled",
+    },
+  ];
+
+  return (
+    <Fragment>
+      <Box px="91px">
+        <Box>
+          <Heading
+            mt="60px"
+            fontFamily="Playfair Display"
+            fontWeight="700"
+            fontSize="32px"
+            color="#021D37"
+          >
+            Post Job Vacancy
+          </Heading>
+
+          <Text
+            fontFamily="Manrope"
+            fontWeight="400"
+            fontSize="24px"
+            lineHeight="33px"
+            color="#021D37"
+            my="24px"
+          >
+            Wavecrest offers accomodation to its students. We have a two well
+            furnished hostels orem ipsum dolor sit amet, consectetur adipiscing
+            elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+            aliqua.
+          </Text>
+        </Box>
+
+        <Box>
+          {forms.map((form, index) => {
+            return (
+              <Input
+                key={index}
+                type={form.fieldType}
+                placeholder={form.placeHolder}
+                variant={form.variant}
+                display="block"
+                w="526px"
+                h="67px"
+                mb="10px"
+                bg="#EBEDEF"
+                _placeholder={{
+                  fontFamily: "Manrope, sans-serif",
+                  color: "#021D37",
+                  fontSize: "18px",
+                  lineHeight: "25px",
+                  fontWeight: "400",
+                }}
+              />
+            );
+          })}
+
+          <Textarea
+            placeholder="Extra Notes"
+            variant="filled"
+            w="526px"
+            h="80px"
+            _placeholder={{
+              fontFamily: "Manrope, sans-serif",
+              color: "#021D37",
+              fontSize: "18px",
+              lineHeight: "25px",
+              fontWeight: "400",
+            }}
+          />
+        </Box>
+
+        <Button
+          fontFamily="Manrope"
+          fontSize="16px"
+          fontWeight="700"
+          m="20px 0"
+          borderRadius="3px"
+          bg="#021D37"
+          py="7px"
+          textAlign="center"
+          color="#FFF"
+          _hover={{
+            bg: "#020E1B",
+          }}
+        >
+          POST
+        </Button>
+
+        <Text fontFamily="Manrope" fontSize="24px" fontWeight="400" my="45px">
+          NB: After payment, a google link would be sent to your mail to
+          complete the registration
+        </Text>
+      </Box>
+    </Fragment>
+  );
+};
+
+export default postJobVacancies;
---
+++ src/Pages/Career/viewJobVacancies.jsx
@@ -0,0 +1,91 @@
+import { Box, Button, Grid, Heading, Text } from "@chakra-ui/react";
+import React from "react";
+
+const viewJobVacancies = () => {
+  const jobVacancies = [
+    {
+      company: "Radisson Blu Hotels & suits",
+      desc: "Front Desk official",
+      deadLine: "31st December 2022",
+    },
+    {
+      company: "Wavecrest College of Hospitality",
+      desc: "Culinary Instructor",
+      deadLine: "31st December 2022",
+    },
+    {
+      company: "Odogwu Bitters",
+      desc: "Inspector",
+      deadLine: "31st December 2022",
+    },
+    {
+      company: "Maggi Star",
+      desc: "Front Desk official",
+      deadLine: "31st December 2022",
+    },
+    {
+      company: "Radisson Blu Hotels & suits",
+      desc: "Front Desk official",
+      deadLine: "31st December 2022",
+    },
+    {
+      company: "Radisson Blu Hotels & suits",
+      desc: "Front Desk official",
+      deadLine: "31st December 2022",
+    },
+  ];
+
+  return (
+    <Box my="90px" px="95px">
+      <Heading
+        my="60px"
+        fontFamily="Playfair Display"
+        fontWeight="700"
+        fontSize="32px"
+      >
+        View Job Vacancies
+      </Heading>
+
+      <Grid gridTemplateColumns="1fr 1fr" mt="30px" gridGap="30px">
+        {jobVacancies.map((vacancy) => {
+          return (
+            <Box p="35px" bg="#EBEDEF">
+              <Heading
+                fontFamily="Playfair Display"
+                fontWeight="700"
+                fontSize="32px"
+                color="#021D37"
+              >
+                {vacancy.company}
+              </Heading>
+
+              <Box my="20px">
+                <Text>Description: {vacancy.desc} </Text>
+                <Text>Deadline: {vacancy.deadLine} </Text>
+              </Box>
+
+              <Button
+                fontFamily="Manrope"
+                fontSize="24px"
+                fontWeight="700"
+                borderRadius="3px"
+                bg="#021D37"
+                py="10px"
+                textAlign="center"
+                color="#FFF"
+                _hover={{
+                  bg: "#020E1B",
+                }}
+              >
+                {" "}
+                APPLY
+              </Button>
+            </Box>
+          );
+        })}
+      </Grid>
+    </Box>
+  );
+};
+
+export default viewJobVacancies;
