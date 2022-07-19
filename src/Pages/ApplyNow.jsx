5aa7f878f869a9595df98b4613678fcb4878835c
Akhigbepeace <akhigbepeace2005@gmail.com>
2022-07-19 20:30:43 +0100

    Reduced amount of file changes

--- src/Pages/ApplyNow.jsx
+++ src/Pages/ApplyNow.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import Navbar from "components/layouts/Navbar";
+import Footer from "./Home/Footer";
+import FooterCopywright from "./Home/FooterCopywright";
+import { Box } from "@chakra-ui/react";
 
 const ApplyNow = () => {
   return (
-    <div>ApplyNow</div>
-  )
-}
+    <Box>
+      <Navbar />
+      Apply Now
+      <Footer />
+      <FooterCopywright />
+    </Box>
+  );
+};
 
-export default ApplyNow
+export default ApplyNow;
--- src/Pages/Career/index.jsx
+++ src/Pages/Career/index.jsx
@@ -1,9 +1,99 @@
-import React from 'react'
+import { Box, Image, Link, Text, Flex } from "@chakra-ui/react";
+import React, { useState } from "react";
+import Navbar from "components/layouts/Navbar";
+import { useParams } from "react-router-dom";
+import about from "assets/Images/About.png";
+import PostJobVacancies from "./postJobVacancies";
+import ViewJobVacancies from "./viewJobVacancies";
+import Footer from "Pages/Home/Footer";
+import FooterCopywright from "Pages/Home/FooterCopywright";
 
-const index = () => {
+const Career = () => {
+  const { id } = useParams();
+
+  const menuLinks = [
+    {
+      id: "postJobVacancies",
+      linkTitle: "POST JOB VACANCIES",
+      content: <PostJobVacancies />,
+    },
+    {
+      id: "viewJobVacancies",
+      linkTitle: "VIEW JOB VACANCIES",
+      content: <ViewJobVacancies />,
+    },
+  ];
+
+  const profile = menuLinks.find(
+    (menu) => menu.id.toLowerCase() === id.toLowerCase()
+  );
+
+  const [activeLink, setActiveLink] = useState(profile || menuLinks[0]);
+
   return (
-    <div>index</div>
-  )
-}
+    <Box>
+      <Navbar />
 
-export default index
+      <Box>
+        <Box h="441px" w="100%" gridGap="68px" position="relative">
+          <Image src={about} h="100%" w="100%" />
+
+          <Box
+            bg="rgba(0, 24, 71, 0.5)"
+            h="100%"
+            w="100%"
+            position="absolute"
+            bottom="0"
+          >
+            <Text
+              color="white"
+              fontFamily="Playfair Display"
+              fontWeight="700"
+              fontSize="60px"
+              textAlign="center"
+              mt="140px"
+            >
+              CARRER
+            </Text>
+          </Box>
+        </Box>
+
+        <Box>
+          <Flex alignItems="center" justifyContent="center">
+            {menuLinks.map((menuLink, index) => {
+              const isActive = activeLink.linkTitle === menuLink.linkTitle;
+              return (
+                <Link
+                  key={index}
+                  m="15px 0"
+                  fontFamily="Manrope"
+                  bg={isActive ? "#021d37" : "#fff"}
+                  color={isActive ? "#EBEDEF" : "#021d37"}
+                  fontWeight="700"
+                  fontSize="30px"
+                  textAlign="center"
+                  border="3px solid #021d37"
+                  lineHeight="40px"
+                  p="20px 97px"
+                  onClick={() => setActiveLink(menuLink)}
+                  _hover={{
+                    textDecoration: "none",
+                    bg: isActive ? "#021d37" : "#F5F5F5",
+                  }}
+                >
+                  {menuLink.linkTitle}
+                </Link>
+              );
+            })}
+          </Flex>
+
+          {activeLink.content}
+        </Box>
+      </Box>
+      <Footer />
+      <FooterCopywright />
+    </Box>
+  );
+};
+
+export default Career;
