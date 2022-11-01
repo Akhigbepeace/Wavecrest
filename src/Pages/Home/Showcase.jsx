153cda88675d94a1a04bc7d87c8be53ffa16c37d
Akhigbepeace <akhigbepeace2005@gmail.com>
2022-11-01 16:51:59 +0100

    Fetched media from cloudinary

--- src/Pages/Home/Showcase.jsx
+++ src/Pages/Home/Showcase.jsx
@@ -10,16 +10,19 @@ import {
 import React, { Fragment } from "react";
 import ReactPlayer from "react-player";
 import { useNavigate } from "react-router-dom";
-import showcase1 from "assets/Images/Showcase1.png";
-import showcase2 from "assets/Images/Showcase2.png";
-import showcase3 from "assets/Images/Showcase3.png";
-import showcase4 from "assets/Images/Showcase4.png";
-import waveVid from "assets/Images/wavevid.mp4";
 import ShowcaseSlider from "components/ShowcaseSlider";
 
-const showcaseGallery = [showcase1, showcase2, showcase4, showcase3];
-
 const Showcase = (props) => {
+  const showcase1 =
+    "https://res.cloudinary.com/wavecrest1/image/upload/v1667060013/Showcase1_box3y3.png";
+  const showcase2 =
+    "https://res.cloudinary.com/wavecrest1/image/upload/v1667060014/Showcase2_xlxynv.jpg";
+  const showcase3 =
+    "https://res.cloudinary.com/wavecrest1/image/upload/v1667059988/Showcase3_dtczi4.png";
+  const showcase4 =
+    "https://res.cloudinary.com/wavecrest1/image/upload/v1667060027/Showcase4_nsyiwy.jpg";
+  const waveVid =
+    "https://res.cloudinary.com/wavecrest1/video/upload/v1667061051/wavevid_bvf8bh.mp4";
   const navigate = useNavigate();
 
   const Redirect = (e) => {
@@ -28,6 +31,7 @@ const Showcase = (props) => {
     navigate("/about/profile", { replace: true });
   };
 
+  const showcaseGallery = [showcase1, showcase2, showcase4, showcase3];
   return (
     <Fragment>
       <Grid
@@ -89,7 +93,7 @@ const Showcase = (props) => {
         <Grid templateColumns="1fr 1fr" gap="8px">
           {showcaseGallery.map((gallery, index) => {
             return (
-              <Box key={index} h="185px">
+              <Box key={index} h="175px">
                 <Image
                   src={gallery}
                   alt="Wavecrest Images"
