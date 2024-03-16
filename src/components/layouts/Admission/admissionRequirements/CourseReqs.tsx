c3c5183f75ffa8b7e47587b0130b7f99a4d01e5d
Akhigbepeace <akhigbepeace2005@gmail.com>
2024-03-16 09:19:29 +0100

    fix: admission modal

--- src/components/layouts/Admission/admissionRequirements/CourseReqs.tsx
+++ src/components/layouts/Admission/admissionRequirements/CourseReqs.tsx
@@ -17,6 +17,8 @@ import ShowModal from "./ShowModal";
 const fileDownload = "/assets/imgs/paperDownload.png";
 
 const CourseReqs = (props: RequirementsProps) => {
+  const { onOpen } = useDisclosure();
+
   const textToShow = props.courseRequirements.map((values, i) => {
     return (
       <UnorderedList key={i}>
